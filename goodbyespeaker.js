5f3e27abbaac9a35d5304399873dc6bdedcbbf35
itsalwayssunnyvale <ventanamosal@gmail.com>
2025-02-08 03:54:22 -0500

    Create goodbyespeaker.js

---
+++ goodbyespeaker.js
@@ -0,0 +1,12 @@
+(function (window) {
+  var goodbyeSpeaker = {};  
+  var speakWord = "Good bye";
+    
+  
+  goodbyeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+}
+
+window.goodbyeSpeaker = goodbyeSpeaker;
+  
+})(window);
